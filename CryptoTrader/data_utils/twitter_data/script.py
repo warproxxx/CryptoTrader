f851e11d664771ba7c5a9db879e3b56d1ec91b94
Ubuntu <virtus@DanielAlgoTest.4zp1ykdb2e4uzbmkscwqb2lijc.ix.internal.cloudapp.net>
2018-05-13 03:05:04 +0000

    Created the virtus branch

--- CryptoTrader/data_utils/twitter_data/script.py
+++ CryptoTrader/data_utils/twitter_data/script.py
@@ -5,6 +5,7 @@ from dateutil.rrule import rrule, DAILY
 import pandas as pd
 import time
 
+import os
 
 def scrape(start_date, end_date, keyword="Bitcoin"):
     
@@ -24,6 +25,68 @@ def scrape(start_date, end_date, keyword="Bitcoin"):
             df = df.append({'ID': tweet.id, 'Tweet': tweet.text, 'Time': tweet.timestamp, 'User': tweet.user, 'Likes': tweet.likes, 'Replies': tweet.replies, 'Retweet': tweet.retweets}, ignore_index=True)
 
         df.to_csv("{}\extracted\{}.csv".format(keyword.lower(), dt.strftime('%Y-%m-%d'), index=False))
+        
+detailsList = []
 
-for d in range(2013, 2018):
-    scrape(date(d,1,1), date(d, 12, 31))
+coinDetails = {}
+coinDetails['name'] = 'Bitcoin'
+coinDetails['start'] = date(2013, 1, 1)
+coinDetails['end'] = date(2018, 5, 12)
+detailsList.append(coinDetails)
+
+coinDetails = {}
+coinDetails['name'] = 'Dashpay'
+coinDetails['start'] = date(2013, 12, 15)
+coinDetails['end'] = date(2018, 5, 12)
+detailsList.append(coinDetails)
+
+coinDetails = {}
+coinDetails['name'] = 'Dogecoin'
+coinDetails['start'] = date(2013, 1, 1)
+coinDetails['end'] = date(2018, 5, 12)
+detailsList.append(coinDetails)
+
+coinDetails = {}
+coinDetails['name'] = 'Ethereum'
+coinDetails['start'] = date(2015, 8, 8)
+coinDetails['end'] = date(2018, 5, 12)
+detailsList.append(coinDetails)
+
+coinDetails = {}
+coinDetails['name'] = 'Litecoin'
+coinDetails['start'] = date(2013, 5, 5)
+coinDetails['end'] = date(2018, 5, 12)
+detailsList.append(coinDetails)
+
+coinDetails = {}
+coinDetails['name'] = 'Monero'
+coinDetails['start'] = date(2014, 5, 21)
+coinDetails['end'] = date(2018, 5, 12)
+detailsList.append(coinDetails)
+
+coinDetails = {}
+coinDetails['name'] = 'Ripple'
+coinDetails['start'] = date(2013, 8, 5)
+coinDetails['end'] = date(2018, 5, 12)
+detailsList.append(coinDetails)
+
+coinDetails = {}
+coinDetails['name'] = 'Stellar'
+coinDetails['start'] = date(2014, 8, 5)
+coinDetails['end'] = date(2018, 5, 12)
+detailsList.append(coinDetails)
+
+for coinDetail in detailsList:
+    
+    current_directory = os.getcwd()
+    final_directory = os.path.join(current_directory, coinDetail['name'].lower())
+
+    if not os.path.exists(final_directory):
+        os.makedirs(final_directory)
+        
+for coinDetail in detailsList:
+    print("Scraping {} Data".format(coinDetail['name']))
+    print("Starting Year: {} Ending Year: {}".format(coinDetail['start'].year, coinDetail['end'].year))
+    
+    for d in range(coinDetail['start'].year, coinDetail['end'].year):
+        scrape(date(d,1,1), date(d, 12, 31), keyword=coinDetail['name'])
