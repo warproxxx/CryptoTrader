4a296d237a5b5635bdb472c972f60ffa11e14de5
warproxxx <daniel.sapkota15@my.northampton.ac.uk>
2018-06-13 08:54:38 +0545

    Created profilescraper module. Not fully tested

--- CryptoTrader/data_utils/twitter_data/profilescraper/profile.py
+++ CryptoTrader/data_utils/twitter_data/profilescraper/profile.py
@@ -2,8 +2,85 @@ from datetime import datetime
 from bs4 import BeautifulSoup
 
 from coala_utils.decorators import generate_ordering
+from twitterscraper.tweet import Tweet
 
-@generate_ordering('username')
+@generate_ordering('username', 'location', 'has_location', 'age', 'is_verified', 'total_tweets', 'total_following', 'total_followers', 'total_likes', 'total_moments', 'total_lists', 'has_avatar', 'has_background', 'is_protected', 'profile_modified', 'tweets')
 class Profile:
-    def __init__(self):
-        pass
+    def __init__(self, username, location, has_location, age, is_verified, total_tweets, total_following, total_followers, total_likes, total_moments, total_lists, has_avatar, has_background, is_protected, profile_modified, tweets):
+        self.username = username
+        self.location = location
+        self.has_location = has_location
+        self.age = age
+        self.is_verified = is_verified
+        self.total_tweets = total_tweets
+        self.total_following = total_following
+        self.total_followers = total_followers
+        self.total_likes = total_likes
+        self.total_moments = total_moments
+        self.total_lists = total_lists
+        self.has_avatar = has_avatar
+        self.has_background = has_background
+        self.is_protected = is_protected
+        self.profile_modified = profile_modified
+        self.tweets = tweets
+
+
+    @classmethod
+    def from_soup(cls, soup):
+        sideBar = soup.find('div', 'ProfileHeaderCard')
+        topBar = soup.find('ul',  'ProfileNav-list')
+
+        location=sideBar.find('div', 'ProfileHeaderCard-location').get_text().strip() or 0
+        has_avatar=0 if 'default_profile_images' in soup.find('img', 'ProfileAvatar-image')['src'] else 1
+
+        try:
+            joined = sideBar.find('span', 'ProfileHeaderCard-joinDateText')['title']
+            age = (datetime.now() - datetime.strptime(joined, "%I:%M %p - %d %b %Y")).days
+        except:
+            age = 0
+
+        try:
+            soup.find('div', 'ProfileCanopy-headerBg').find('img')['src']
+            has_background = 1
+        except:
+            has_background = 0
+
+        try:
+            a = soup.find('h2', 'ProtectedTimeline-heading')
+            protected = 1
+        except:
+            protected = 0
+
+
+        tweets = soup.find_all('div', 'tweet')
+        all_tweets = []
+
+        for tweet in tweets:
+            all_tweets.append(Tweet.from_soup(tweet))
+
+        return cls(
+            username=sideBar.find('span', 'username').text or "",
+            location=location,
+            has_location=0 if location == 0 else 1,
+            age=age,
+            is_verified=0 if sideBar.find('span', 'Icon--verified') == None else 1,
+            total_tweets=topBar.find('li', 'ProfileNav-item--tweets').find('span', 'ProfileNav-value')['data-count'] or 0,
+            total_following=topBar.find('li', 'ProfileNav-item--following').find('span', 'ProfileNav-value')['data-count'] or 0,
+            total_followers=topBar.find('li', 'ProfileNav-item--followers').find('span', 'ProfileNav-value')['data-count'] or 0,
+            total_likes=topBar.find('li', 'ProfileNav-item--favorites').find('span', 'ProfileNav-value')['data-count'] or 0,
+            total_moments=topBar.find('li', 'ProfileNav-item--moments').find('span', 'ProfileNav-value')['data-count'] or 0,
+            total_lists=topBar.find('li', 'ProfileNav-item--lists').find('span', 'ProfileNav-value')['data-count'] or 0,
+            has_avatar=has_avatar,
+            has_background=has_background,
+            is_protected=protected,
+            profile_modified=1 if has_background == 1 or has_avatar == 1 or location != 0 else 0,
+            tweets=all_tweets
+        )
+
+    @classmethod
+    def from_html(cls, html):
+        soup = BeautifulSoup(html, "lxml")
+        try:
+            yield cls.from_soup(soup)
+        except AttributeError:
+            pass
--- CryptoTrader/data_utils/twitter_data/profilescraper/query.py
+++ CryptoTrader/data_utils/twitter_data/profilescraper/query.py
@@ -4,17 +4,50 @@ import json
 import random
 import logging
 
+from functools import partial
+
+from profilescraper.profile import Profile
+
 HEADERS_LIST = ['Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.79 Safari/537.36', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:60.0) Gecko/20100101 Firefox/60.0', 'Mozilla/5.0 (X11; Linux x86_64; rv:58.0) Gecko/20100101 Firefox/58.0']
 
-def query_single_profile(url, retries=10, proxies=None):
+def query_single_profile(url, retry=10, proxies=None):
     headers = {'User-Agent': random.choice(HEADERS_LIST)}
 
-    if (proxies == None):
-        response = requests.get(url, headers=headers)
-    else:
-        response = requests.get(url, headers=headers, proxies=proxies)
+    try:
+        if (proxies == None):
+            response = requests.get(url, headers=headers)
+        else:
+            response = requests.get(url, headers=headers, proxies=proxies)
 
-    html = response.text or ''
+        html = response.text or ''
+
+        profiles = list(Profile.from_html(html))
+
+        if not profiles:
+            return []
+
+        return profiles
+    except requests.exceptions.HTTPError as e:
+        logging.exception('HTTPError {} while requesting "{}"'.format(
+            e, url))
+    except requests.exceptions.ConnectionError as e:
+        logging.exception('ConnectionError {} while requesting "{}"'.format(
+            e, url))
+    except requests.exceptions.Timeout as e:
+        logging.exception('TimeOut {} while requesting "{}"'.format(
+            e, url))
+    except json.decoder.JSONDecodeError as e:
+        logging.exception('Failed to parse JSON "{}" while requesting "{}".'.format(
+            e, url))
+    except ValueError as e:
+        logging.exception('Failed to parse JSON "{}" while requesting "{}"'.format(e, url))
+
+    if retry > 0:
+        logging.info("Retrying... (Attempts left: {})".format(retry))
+        return query_single_profile(url, retry-1, proxies=proxies)
+    
+    logging.error("Giving up.")
+    return []
 
 
 def query_profile(profiles, poolsize=20, proxies=None):
@@ -26,6 +59,26 @@ def query_profile(profiles, poolsize=20, proxies=None):
     Size of pool. Bigger - the more instance of browser is opened
     '''
     url = "https://twitter.com/{}"
-    
+    no_profiles = len(profiles)
 
+    if (poolsize > no_profiles):
+        poolsize = no_profiles
+
+    urls = [url.format(x) for x in profiles]
+
+    all_profile = []
+
+    pool = Pool(poolsize)
+
+    try:
+        for profile_data in pool.imap_unordered(partial(query_single_profile, proxies=proxies), urls):
+            all_profile.extend(profile_data)
+            logging.info("Got {} profiles ({} new).".format(
+                len(all_profile), len(profile_data)))
+
+    finally:
+        pool.close()
+        pool.join()
+
+    return all_profile
     
