e51e7f73c34472e3aa09167de3603ec06ef14178
Daniel Sapkota <n00bdan13@gmail.com>
2018-07-09 10:10:04 +0000

    Updated repository structure to move tests directory

---
+++ src/data_utils/twitter_data/proxy_utils.py
@@ -0,0 +1,25 @@
+import os
+
+def proxy_dict(address):
+    http_proxy  = "http://{}".format(address)
+    https_proxy = "https://{}".format(address)
+    ftp_proxy   = "ftp://{}".format(address)
+
+    proxyDict = { 
+                  "http"  : http_proxy, 
+                  "https" : https_proxy, 
+                  "ftp"   : ftp_proxy
+                }
+    
+    return proxyDict
+
+def get_proxies():
+    a = open(os.path.dirname(__file__.replace('/proxy_utils.py', '')) + '/proxies.txt')
+
+    proxies = []
+
+    for line in a.readlines():
+        proxy = proxy_dict(line.replace('\n', ''))
+        proxies.append(proxy)
+        
+    return proxies
