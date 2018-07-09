bfcca213a4b5ef0d6c74d484d998118bf5701cf5
Daniel Sapkota <n00bdan13@gmail.com>
2018-07-09 10:25:08 +0000

    Updated folders to add tests

--- src/data_utils/tests/test_functions.py
+++
@@ -1,9 +0,0 @@
-from data_utils import PriceFunctions
-import numpy as np
-import pandas as pd
-        
-class TestPriceFunctions():   
-
-    def setup_method(self, test_method):
-        self.pf = PriceFunctions()
-        self.coins = ['BTC', 'ETH', 'DASH', 'DOGE', 'LTC', 'STR', 'XMR', 'XRP']
