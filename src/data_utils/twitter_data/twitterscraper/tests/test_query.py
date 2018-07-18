d9f94272eb1a20d51701f1889869ede20583c170
Daniel Sapkota <daniel@quantorithm.com>
2018-07-18 11:40:34 +0545

    Added all tests to twitterscraper

--- src/data_utils/twitter_data/twitterscraper/tests/test_query.py
+++ src/data_utils/twitter_data/twitterscraper/tests/test_query.py
@@ -1,2 +1,15 @@
-from twitterscraper import query_tweets
+from twitterscraper.query import query_single_page, query_tweets
 
+def test_query_single_page():
+    tweets, ids = query_single_page("https://twitter.com/search?vertical=tweets&vertical=default&q=Bitcoin&l=EN")
+    
+    for tweet in tweets:
+        assert(int(tweet.retweets) >= 0)
+
+def test_query_tweets():
+    tweets = query_tweets("Bitcoin", limit=50)
+
+    assert(len(tweets) > 50)
+
+    for tweet in tweets:
+        assert(int(tweet.replies) >= 0)
