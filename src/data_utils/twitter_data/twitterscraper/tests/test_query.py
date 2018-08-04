9c9c1b7cc8e405dc7779d6a5006197d1b16c30c1
Daniel Sapkota <daniel@quantorithm.com>
2018-08-04 19:27:50 +0545

    added tests for twitterscraper

--- src/data_utils/twitter_data/twitterscraper/tests/test_query.py
+++ src/data_utils/twitter_data/twitterscraper/tests/test_query.py
@@ -9,14 +9,41 @@ class TesttwitterScraper:
         
         for tweet in tweets:
             assert(int(tweet.retweets) >= 0)
+            assert(len(str(tweet.id)) >= 18)
+            assert(int(tweet.likes) >= 0)
+            assert(int(tweet.replies) >= 0)
+            assert(tweet.reply_to_id)
+            assert(any(x in tweet.response_type for x in ['tweet', 'retweet', 'quoted_status', 'quoted_retweet', 'reply']))
+            assert(tweet.text)
+            assert(len(str(tweet.timestamp)) >= 10)
+            assert(len(tweet.user.split(' ')) == 1)
 
     def test_query_tweets_once(self):
-        print(self.ts.query_tweets_once("Bitcoin", limit=10))
+        tweets = self.ts.query_tweets_once("Bitcoin", limit=10)
+
+        for tweet in tweets:
+            assert(int(tweet.retweets) >= 0)
+            assert(len(str(tweet.id)) >= 18)
+            assert(int(tweet.likes) >= 0)
+            assert(int(tweet.replies) >= 0)
+            assert(tweet.reply_to_id)
+            assert(any(x in tweet.response_type for x in ['tweet', 'retweet', 'quoted_status', 'quoted_retweet', 'reply']))
+            assert(tweet.text)
+            assert(len(str(tweet.timestamp)) >= 10)
+            assert(len(tweet.user.split(' ')) == 1)
 
     def test_query_historic_tweets(self):
         tweets = self.ts.query_historic_tweets("Bitcoin", limit=50)
 
-        assert(len(tweets) > 50)
+        assert(len(tweets) >= 50)
 
         for tweet in tweets:
+            assert(int(tweet.retweets) >= 0)
+            assert(len(str(tweet.id)) >= 18)
+            assert(int(tweet.likes) >= 0)
             assert(int(tweet.replies) >= 0)
+            assert(tweet.reply_to_id)
+            assert(any(x in tweet.response_type for x in ['tweet', 'retweet', 'quoted_status', 'quoted_retweet', 'reply']))
+            assert(tweet.text)
+            assert(len(str(tweet.timestamp)) >= 10)
+            assert(len(tweet.user.split(' ')) == 1)
